e91e0fb37849ea791cf90fc56e6de4f9c025c0bb
Tom TREILLON <TREILLON_To@lprs.local>
2026-03-30 16:42:51 +0200

    [feat]Début du projet

---
+++ src/main/java/appli/StartApplication.java
@@ -0,0 +1,22 @@
+package appli;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("accueil/LoginView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch(StartApplication.class, args);
+    }
+}
---
+++ src/main/java/appli/accueil/LoginController.java
@@ -0,0 +1,14 @@
+package appli.accueil;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class LoginController {
+    @FXML
+    private TextField fieldEmail;
+    @FXML
+    private TextField fieldMDP;
+    @FXML
+    private Text textErreur;
+}
