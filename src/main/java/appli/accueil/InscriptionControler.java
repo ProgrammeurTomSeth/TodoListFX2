6bb36f9d3a859bcd5ebcc9c71a0c3e2561d85ad6
Tom TREILLON <TREILLON_To@lprs.local>
2026-03-30 17:59:29 +0200

    [feat]Ajout de la page d'inscription et du changement de pages

--- src/main/java/appli/accueil/InscriptionControler.java
+++ src/main/java/appli/accueil/InscriptionControler.java
@@ -1,4 +1,17 @@
 package appli.accueil;
 
+import appli.StartApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class InscriptionControler {
+    @FXML
+    private Button retour;
+    @FXML
+    void retour(ActionEvent event) throws IOException {
+        StartApplication.changeScene("accueil/Login");
+    }
 }
--- src/main/java/appli/accueil/LoginController.java
+++ src/main/java/appli/accueil/LoginController.java
@@ -1,14 +1,49 @@
 package appli.accueil;
 
+import appli.StartApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
+
 public class LoginController {
+    @FXML
+    private Button BtConnexion;
+    @FXML
+    private Button BtInscri;
+    @FXML
+    private Button BtMdpOu;
     @FXML
     private TextField fieldEmail;
     @FXML
-    private TextField fieldMDP;
+    private PasswordField fieldMDP;
     @FXML
     private Text textErreur;
+    @FXML
+    void Connexion(ActionEvent event) {
+        String email = fieldEmail.getText();
+        String mdp = fieldMDP.getText();
+        System.out.println("Email: " + email);
+        System.out.println("MDP: " + mdp);
+        if (email.isEmpty() || mdp.isEmpty()) {
+            textErreur.setText("Tous les champs sont obligatories !");
+        } else if (email.equals("t.treillon@orange.fr") && mdp.equals("Azerty1234")) {
+            textErreur.setText("Vous êtes connecté !");
+        }else {
+            textErreur.setText("Les informations fournis ne suffisent pas à vous authentifier");
+        }
+    }
+    @FXML
+    void Inscription(ActionEvent event) throws IOException {
+        StartApplication.changeScene("accueil/Inscription");
+    }
+    @FXML
+    void MdpOublier(ActionEvent event) {
+
+    }
+
 }
