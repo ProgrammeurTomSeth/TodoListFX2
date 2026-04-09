944aa2f959d20b3e1702cd2002f9f617b0dd62f2
Tom TREILLON <TREILLON_To@lprs.local>
2026-04-09 15:41:49 +0200

    [feat]Ajout des class Utilisateur et son Repository + création des informations de l'utilisateur et de la connexion avec la BDD

---
+++ src/main/java/model/Utilisateur.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Utilisateur {
+    int id_utilisateur;
+    String prenom;
+    String nom;
+    String email;
+    String mdp;
+
+    public int getId_utilisateur() {
+        return id_utilisateur;
+    }
+    public void setId_utilisateur(int id_utilisateur) {
+        this.id_utilisateur = id_utilisateur;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getMdp() {
+        return mdp;
+    }
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public Utilisateur(int id_utilisateur, String prenom, String nom, String email, String mdp) {
+        this.id_utilisateur = id_utilisateur;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.email = email;
+        this.mdp = mdp;
+    }
+    public Utilisateur(String prenom, String nom, String email, String mdp) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.email = email;
+        this.mdp = mdp;
+    }
+    public Utilisateur(String email, String mdp) {
+        this.email = email;
+        this.mdp = mdp;
+    }
+    public String toString() {
+        return prenom + " " + nom + " : " + email + " ; " + mdp;
+    }
+}
